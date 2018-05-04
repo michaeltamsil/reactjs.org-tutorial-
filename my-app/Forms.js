5f20298a9b2d1dcf228102fabe332b4cf012dc28
michael tamsil <michaeltamsil@gmail.com>
2018-05-04 16:41:51 +0700

    update latihan reactjs

--- my-app/Forms.js
+++ my-app/Forms.js
@@ -5,3 +5,10 @@
     </label>
     <input type="submit" value="Submit" />
 </form>
+
+
+ReactDOM.render(<input value="hi" />, mountNode);
+
+setTimeout(function() {
+    ReactDOM.render(<input value={null} />, mountNode);
+}, 1000)
